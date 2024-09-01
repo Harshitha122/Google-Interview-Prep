8abc630a275034d5a6f7553811ba32c40753b180
Harshitha122 <71165978+Harshitha122@users.noreply.github.com>
2024-09-01 23:21:59 +0530

    Create file

---
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int l=0;
+        int r=m*n-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(matrix[mid/n][mid%n]==target){
+                return true;
+            }
+            else if(matrix[mid/n][mid%n]>target){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return false;
+    }
+};
