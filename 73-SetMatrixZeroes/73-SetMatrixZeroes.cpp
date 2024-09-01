ab1e169c0719de0c6219715d77b49cc98624b698
Harshitha122 <71165978+Harshitha122@users.noreply.github.com>
2024-09-01 23:15:13 +0530

    Update file 01/09/2024, 23:15:12

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.cpp
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int col0=0;
-        for(int i=0;i<m;i++){
-            if(matrix[i][0]==0){
-                col0=1;
+        bool isCol = false;
+        int R = matrix.size();
+        int C = matrix[0].size();
+        for (int i = 0; i < R; i++) {
+            if (matrix[i][0] == 0) {
+                isCol = true;
             }
-        }
-        for(int i=0;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0]=matrix[0][j]=0;
+            for (int j = 1; j < C; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=1;j--){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
-                    matrix[i][j]=0;
+        for (int i = 1; i < R; i++) {
+            for (int j = 1; j < C; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
             }
-                if(col0==1){
-                    matrix[i][0]=0;
-                }
-            
+        }
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < C; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (isCol) {
+            for (int i = 0; i < R; i++) {
+                matrix[i][0] = 0;
+            }
         }
     }
 };
