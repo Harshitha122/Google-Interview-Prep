f9c7e2c36680e2146292bea84ef4c876b8338a0e
Harshitha122 <71165978+Harshitha122@users.noreply.github.com>
2024-09-17 00:26:01 +0530

    Create file

---
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n1 = s.length();
+        int n2 = p.length();
+        vector<vector<bool>> dp(n1+1, vector<bool>(n2+1, false));
+        dp[0][0] = true;
+        
+        for (int j = 1; j <= n2; j++) {
+            if (p[j-1] == '*' && dp[0][j-2]) {
+                dp[0][j] = true; 
+            }
+        }
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (p[j-1] == s[i-1] || p[j-1] == '.') {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if (p[j-1] == '*') {dp[i][j] = dp[i][j-2];
+                   if (p[j-2] == s[i-1] || p[j-2] == '.') {
+                        dp[i][j] = dp[i][j] || dp[i-1][j];
+                    }
+                }
+            }
+        }
+        
+        return dp[n1][n2];
+    }
+};
