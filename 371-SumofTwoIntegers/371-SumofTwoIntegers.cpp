fddd23e9d476ea274f2402a23558e0616fa2d367
Harshitha122 <71165978+Harshitha122@users.noreply.github.com>
2024-09-22 00:17:39 +0530

    Create file

---
+++ 371-SumofTwoIntegers/371-SumofTwoIntegers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while(b!=0)
+        {
+            int carry=((unsigned)(a&b)) << 1;
+            a=a^b;
+            b=carry;
+        }
+        return a;
+    }
+};
